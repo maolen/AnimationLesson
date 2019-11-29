ece618284e5648ccce425ee901775f5af5a3a176
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-11-29 18:50:09 +0600

    Создан свой прогресс бар

---
+++ AnimationLesson/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimationLesson
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Load(object sender, RoutedEventArgs e)
+        {
+            //var animation = new DoubleAnimation();
+            //animation.From = loadRectangle.ActualWidth;
+            //animation.To = ActualWidth;
+            //animation.Duration = TimeSpan.FromSeconds(10);
+            //animation.RepeatBehavior = RepeatBehavior.Forever;
+            //loadRectangle.BeginAnimation(Rectangle.WidthProperty, animation);
+        
+            var animation = new DoubleAnimation();
+            animation.From = double.IsNaN(Canvas.GetLeft(loadRectangle)) ? 0 : Canvas.GetLeft(loadRectangle);
+            animation.To = ActualWidth;
+            animation.Duration = TimeSpan.FromSeconds(10);
+            animation.RepeatBehavior = RepeatBehavior.Forever;
+            loadRectangle.BeginAnimation(Canvas.LeftProperty, animation);
+        }
+    }
+}
